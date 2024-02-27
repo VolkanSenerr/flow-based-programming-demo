8c5839c678fe3a9b8159527f2832a4c4c9419d55
Volkan Şener <senerr.volkan@gmail.com>
2024-02-27 22:09:29 +0300

    Uses new blocks, outputs response

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from barfi import st_barfi, barfi_schemas
 import streamlit as st
 
-from blocks import loader, selector
+from blocks import loader, selector, minmax, result
 
-base_blocks = [loader, selector]
+base_blocks = [loader, selector, minmax, result]
 
 
 barfi_schema_name = st.selectbox(
@@ -14,5 +14,8 @@ compute_engine = st.checkbox('Activate barfi compute engine', value=True)
 barfi_result = st_barfi(base_blocks=base_blocks, compute_engine=compute_engine, load_schema=barfi_schema_name)
 
 if barfi_result:
-    st.write(barfi_result['CSV Loader']['block'].get_interface(name='CSV Dosya Yükle'))
-    st.write(barfi_result['Select CSV']['block'].get_interface(name='CSV Dosya Seç'))
+    res = barfi_result['Display Result-1']['block'].get_interface(name='disp_input')[0]
+    st.write("Min: " + res)
+    res = barfi_result['Display Result-2']['block'].get_interface(name='disp_input')[0]
+    st.write("Max:" + res)
+
